c8555df4eb4c689f9dee0a7b237d802d34cb40d6
pierav <pierre.ravenel@univ-grenoble-alpes.fr>
2023-05-18 13:30:46 +0200

    Fix strncmp implem

--- src/std/string.c
+++ src/std/string.c
@@ -51,11 +51,15 @@ size_t strnlen(const char *s, size_t maxlen) {
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-    while (n-- > 0 && *s1 && *s2 && *s1 == *s2) {
-        s1++;
-        s2++;
-    }
-    return *s1 - *s2;
+    if (n == 0)
+        return (0);
+    do {
+        if (*s1 != *s2++)
+            return (*(unsigned char *)s1 - *(unsigned char *)--s2);
+        if (*s1++ == 0)
+            break;
+    } while (--n != 0);
+    return (0);
 }
 
 char *strdup(const char *s) {
